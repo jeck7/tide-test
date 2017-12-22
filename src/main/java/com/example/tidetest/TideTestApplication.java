f5c0dccdfeb8b8cfdf376a3b609090e8d65c73cb
user <hristoxs@gmail.com>
2017-12-22 20:43:50 +0200

    1

--- src/main/java/com/example/tidetest/TideTestApplication.java
+++ src/main/java/com/example/tidetest/TideTestApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class TideTestApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TideTestApplication.class, args);
     }
